b48bcc06af63e05d7678056824a789e59a323272
0xmitsuri <0xmitsuri@gmail.com>
2025-06-29 07:32:07 +0530

    react router dom, motion, tweakcn t3 chat theme add.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Anonymous Message App</h1>
+      <Button>Button</Button>
     </div>
   )
 }
